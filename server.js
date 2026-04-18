0514994e38157e7c32042088ee2b3f27abec9013
dog684331-prog <dog684331@gmail.com>
2026-04-18 23:14:54 +0600

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const ENABLE_THINKING_MODE = false; // Set to true to enable chat_template_kwarg
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'nvidia/llama-3.1-nemotron-ultra-253b-v1',
   'gpt-4': 'qwen/qwen3-next-80b-a3b-thinking',
-  'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
+  'gpt-4-turbo': 'moonshotai/kimi-k2.5',
   'gpt-4o': 'qwen/qwen3-coder-480b-a35b-instruct',
   'claude-3-opus': 'deepseek-ai/deepseek-v3.1-terminus',
   'claude-3-sonnet': 'deepseek-ai/deepseek-v3.2',
